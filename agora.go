56c63b0aaf93a0abe731bf565bbe98931007b0e1
Ethan Hicks <ehicks@berkeley.edu>
2022-01-22 14:38:36 -0800

    init agora

---
+++ agora.go
@@ -0,0 +1,7 @@
+package main
+
+func main() {
+
+	// this is not really for anything but to act as a github file making sure I set up the depencencies correcly
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module Agora
+
+go 1.17
