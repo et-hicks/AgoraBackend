d1c96e2a7faf51b3b35b266656111f67063b916a
Ethan Hicks <ehicks@berkeley.edu>
2022-01-25 00:32:13 -0800

    Now we have everything in place and we can now begin development

--- agora.go
+++ agora.go
@@ -1,9 +1,22 @@
 package main
 
+import (
+	"fmt"
+	"github.com/admin-agora/backend/protobufs/protobufs"
+)
+
 func main() {
 
 	// this is not really for anything but to act as a github file making sure I set up the depencencies correcly
 	// ok good
+	person := protobufs.Person{
+		Name:        "",
+		Id:          0,
+		Email:       "",
+		Phones:      nil,
+		LastUpdated: nil,
+	}
 
+	fmt.Println(person)
 
 }
