2b360fdf0d65a68c5a45562f9fc3c82ddd7b0db7
Albert Wan <albertwan@Alberts-MacBook-Pro.local>
2022-01-29 13:13:40 -0800

    tthread/commnet/user sql create tables

---
+++ sql/db/V1.users.sql
@@ -0,0 +1,14 @@
+CREATE TABLE threads (
+    id int NOT NULL
+    createdTime time NOT NULL
+    lastUpdatedTime time NOT NULL
+    firstName text(255) NULL
+    lastName text(255) NULL
+    userName char(82) NOT NULL
+    email varchar(255) NOT NULL
+    password char(82) NOT NULL
+    phoneNumber int NOT NULL
+    phoneCode int NOT NULL
+    AccountType text(255) NOT NULL
+    fefs json NOT NULL
+)
