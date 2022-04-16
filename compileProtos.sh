5508fbd96698dc89fb7a317de1dd0ca3fd8cc85e
Ethan Hicks <ehicks@berkeley.edu>
2022-04-16 01:33:07 -0700

    nice

--- compileProtos.sh
+++ compileProtos.sh
@@ -1,13 +1,14 @@
 #!/bin/zsh
 
 echo "proto system paths are:"
-echo "\t" $AGORA_PROTO_DIR
-echo "\t" $AGORA_PROTO_DST_DIR
+echo "\t" "$AGORA_PROTO_DIR"
+
+echo "\t" "$AGORA_PROTO_DST_DIR"
 
 # remove old compiled files
 cd protobufs && rm -rf protobufs
 
-protoc -I=$AGORA_PROTO_DIR --go_out=$AGORA_PROTO_DST_DIR $AGORA_PROTO_DIR/*.proto
+protoc -I="$AGORA_PROTO_DIR" --go_out="$AGORA_PROTO_DST_DIR" "$AGORA_PROTO_DIR"/*.proto
 
 go mod tidy # TODO: figure out how to make this tidy work
 
